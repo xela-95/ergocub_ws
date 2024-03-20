e68d4230c3b53df500076778191878f280d6db7f
Alessandro Croci <alessandro.croci@iit.it>
2024-03-20 15:21:52 +0100

    Update to make the walking-controller work
    See https://github.com/robotology/gz-sim-yarp-plugins/issues/122#issuecomment-2009670509

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 export GZ_SIM_RESOURCE_PATH=${GZ_SIM_RESOURCE_PATH}:/home/acroci/ergocub_ws/install/share/
 export YARP_ROBOT_NAME="ergoCubGazeboV1_1"
 export YARP_DATA_DIRS=${YARP_DATA_DIRS}:/home/acroci/ergocub_ws/install/share/ergoCub
+# export YARP_CLOCK=/clock
