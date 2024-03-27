4e8973495151fcd9b91648148b83e2d73df0253a
Alessandro Croci <alessandro.croci@iit.it>
2024-03-27 17:37:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-export GZ_SIM_RESOURCE_PATH=${GZ_SIM_RESOURCE_PATH}:/home/acroci/ergocub_ws/install/share/
+export GZ_SIM_RESOURCE_PATH=${GZ_SIM_RESOURCE_PATH}:${CONDA_PREFIX}/share/
+export GZ_SIM_SYSTEM_PLUGIN_PATH=${GZ_SIM_SYSTEM_PLUGIN_PATH}:${CONDA_PREFIX}/lib
+
 export YARP_ROBOT_NAME="ergoCubGazeboV1_1"
-export YARP_DATA_DIRS=${YARP_DATA_DIRS}:/home/acroci/ergocub_ws/install/share/ergoCub
+export YARP_DATA_DIRS=${YARP_DATA_DIRS}:${CONDA_PREFIX}/share/ergoCub
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${CONDA_PREFIX}/share:${CONDA_PREFIX}/share/ergoCub/robots
 # export YARP_CLOCK=/clock
